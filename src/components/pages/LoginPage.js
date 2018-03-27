a3b3889abfd348624cfd83db25524da7d2914e91
Naveen <naveen.veenu@gmail.com>
2018-03-27 15:41:56 +0530

    semantic ui setup

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => (
 	<div>
 		<h1> Login Page</h1>
-		<h1> Login Page</h1>
+		<Link to='/'>Go to HomePage</Link>
 	</div>
 
 	);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>, 
+	document.getElementById('root')
+);
 registerServiceWorker();
